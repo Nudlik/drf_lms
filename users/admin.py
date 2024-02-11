62329a971a6bd234821de86e1234a723a7f8af84
Nudlik <gegorp@mail.ru>
2024-02-11 17:16:52 +0700

    корректировка админки для Курс, Урок, Пользователь

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,53 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['email', 'first_name', 'last_name', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups']
+    search_fields = ['first_name', 'last_name', 'email']
+    exclude = ['password']
+    filter_horizontal = [
+        'groups',
+        'user_permissions',
+    ]
+    save_on_top = True
+    fieldsets = (
+        (
+            _('Personal info'),
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'phone',
+                    'city',
+                )
+            }
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                )
+            }
+        ),
+    )
